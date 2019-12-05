65095997f4563acd580976575ac425909f99d85b
okFox <smayfield@gmail.com>
2019-12-04 16:37:30 -0800

    finished

--- __tests__/array-shift.test.js
+++ __tests__/array-shift.test.js
@@ -5,7 +5,9 @@ const {
 
 describe('array-shift', () => {
   it('takes a value and inserts in the middle of an array', () => {
+    console.log(insertShiftArray([2, 4, 6, 8], 5));
     expect(insertShiftArray([2, 4, 6, 8], 5)).toEqual([2, 4, 5, 6, 8]);
-    expect(insertShiftArray([4, 8, 15, 23, 42], 16)) .toEqual([4, 8, 15, 16, 23, 42]);
+    console.log(insertShiftArray([4, 8, 15, 23, 42], 16));
+    expect(insertShiftArray([4, 8, 15, 23, 42], 16)).toEqual([4, 8, 15, 16, 23, 42]);
   });
 });
