bb13b565418683ad8878e3b14bf85a0f29ad2ac3
okFox <smayfield@gmail.com>
2019-12-20 15:20:17 -0800

    about to delete commented code

--- challenges/data structures/queue-with-stacks.js
+++ challenges/data structures/queue-with-stacks.js
@@ -7,11 +7,13 @@ class Node {
   
 class Stack {
   constructor(value) { //maybe you can remove null?  should always have value for later checks
-    this.top = new Node(value);
+    this.top = value ? new Node(value) : null;
   }
   
   push(value) { 
+    const top = this.top;
     this.top = new Node(value);
+    this.top.next = top;
   }
   
   pop() {
@@ -28,42 +30,45 @@ class Stack {
 class PseudoQueue {
   constructor(value) {
     this.pushStack = new Stack(value); //what if you created both stacks with very first value?
-    this.pullStack = new Stack(value = null);
+    this.pullStack = new Stack(null);
   }
 
   //which inserts value into the PseudoQueue, using a first-in, first-out approach.
   enqueue(value) {
-    if(this.pullStack.top !== null){
+    // if(this.pullStack.top !== null){
       
-      this.resetStacks();
-    }
+    //   this.resetStacks();
+    // }
     this.pushStack.push(value);
   }
 
   //which inserts value into the PseudoQueue, using a first-in, first-out approach.
   dequeue() {
-    if(this.pullStack !== null) { //if there is still something in the pullstack
-      return (this.pullStack).pop();
+    if(this.pullStack.top !== null) { //if there is still something in the pullstack
+      return this.pullStack.pop();
     } else {    
-      //when pullstack is empty, push everything from pushstack to pullstack and return top of pushstack
-      let current = this.pushStack.top;
-      while(current.value !== null) {
+      while(this.pushStack.top !== null) {
         let jump = this.pushStack.pop();
-        this.pullStack.push(jump); 
-      }
-      return (this.pushStack).pop();
+        this.pullStack.push(jump);
+      //when pullstack is empty, push everything from pushstack to pullstack and return top of pushstack
+      // let current = this.pushStack.top;
+      // while(current.value !== null) {
+      //   let jump = this.pushStack.pop();
+      //   this.pullStack.push(jump); 
+      // }
+      
     }
+    return this.pullStack.pop();
   }
 
-  resetStacks() {
-    //this fills up the pullStack again and now the nodes are in the right order
-    let current = this.pushStack.top;
-    while(current !== null) {
-      let jump = this.pushStack.pop();
-      this.pushStack.top = current.next;
-      this.pullStack.push(jump);
-    } 
-  }
+  // resetStacks() {
+  //   //this fills up the pullStack again and now the nodes are in the right order
+
+  //   while(this.pushStack.top !== null) {
+  //     let jump = this.pushStack.pop();
+  //     this.pullStack.push(jump);
+  //   } 
+  // }
 }
 
 
